3043a64fc84a8a157f230e85980f592e753bfd05
nawanop.k <nawanop.kaj@thinkerfint.com>
2024-09-06 04:20:10 +0700

    change server.ts code

--- server.ts
+++ server.ts
@@ -1,20 +1,21 @@
-import express, { Request, Response } from 'express';
-import next from 'next';
- 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
- 
-app.prepare().then(() => {
-    const server = express();
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
 
-    server.all('*', (req: Request, res: Response) => {
-      return handle(req, res);
-    });
-  
-    server.listen(port, (err?: any) => {
-      if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
-    });
-  });
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`
+  );
+});
